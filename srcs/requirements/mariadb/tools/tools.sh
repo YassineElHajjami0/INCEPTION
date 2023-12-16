f8d48027af74b9db673ea1d4888bed09df252ae7
Yassine El hajjami <yel-hajj@e1r10p14.1337.ma>
2023-12-16 12:34:21 +0100

    it's working

--- srcs/requirements/mariadb/tools/tools.sh
+++ srcs/requirements/mariadb/tools/tools.sh
@@ -1,22 +1,34 @@
-#!/bin/sh
-/etc/init.d/mariadb start
-sleep 5
+#!/bin/bash
 
+# Start MySQL in the background
+mysqld_safe &
+
+# Wait for MySQL to be ready
+while ! mysqladmin ping --silent; do
+    sleep 1
+    echo "Waiting for MySQL to be ready..."
+done
+
+# Create SQL script file
 echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" > /tmp/file.sql
-echo "CREATE USER IF NOT EXISTS $SQL_USER@localhost IDENTIFIED BY $SQL_PASSWORD;" >> /tmp/file.sql
-echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" > /tmp/file.sql
-echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" > tmp/file.sql
-echo "CREATE USER IF NOT EXISTS $SQL_USER@localhost IDENTIFIED BY $SQL_PASSWORD;"
-echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO $SQL_USE@% IDENTIFIED BY $SQL_PASSWORD;"
-mysql -e "FLUSH PRIVILEGES;"
+echo "CREATE USER 'root'@'%' IDENTIFIED BY '$SQL_ROOT_PASSWORD';" >> /tmp/file.sql
+echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';" >> /tmp/file.sql
+echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';" >> /tmp/file.sql
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' with grant option;" >> /tmp/file.sql
+echo "FLUSH PRIVILEGES;" >> /tmp/file.sql
 
-# Update root user password
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';"
+# Print the content of the SQL script for debugging
+echo "SQL Script:"
+cat /tmp/file.sql
 
-# Shutdown MariaDB gracefully
-# mysql -u root -p"$SQL_ROOT_PASSWORD" --socket=/run/mysqld/mysqld.sock
+# Execute the SQL script
+echo "Executing SQL script..."
+mysql -u root -p"$SQL_ROOT_PASSWORD" < /tmp/file.sql
+
+# Shutdown MySQL
+echo "Shutting down MySQL..."
 mysqladmin -u root -p"$SQL_ROOT_PASSWORD" shutdown
 
-# Start mysqld in the foreground to keep the container running
-exec mysqld
-#mariadb % docker run -it --env-file=.env mariadb bash
+# Start MySQL again
+echo "Starting MySQL again..."
+mysqld_safe
--- srcs/requirements/wordpress/tools/tools.sh
+++ srcs/requirements/wordpress/tools/tools.sh
@@ -1,22 +1,31 @@
 #!/bin/sh
 
-sleep 5
+sleep 20
 
 cd /var/www/html
-wp core download  --allow-root
-wp config create  --dbname=$SQL_DATABASE--dbuser=$SQL_USER--dbpass=$SQL_PASSWORD--dbhost=$DB_HOST	--allow-root --path="/var/www/html"
+
+sed -i 's/^listen = .*/listen = 0.0.0.0:9000/' /etc/php/7.4/fpm/pool.d/www.conf
+
+if [ ! -e index.php ]; then
+    wp core download --allow-root
+fi
+
+# wp core download  --allow-root
+if [ ! -e wp-config.php ]; then
+    wp config create --dbname=$SQL_DATABASE \
+                     --dbuser=$SQL_USER \
+                     --dbpass=$SQL_PASSWORD \
+                     --dbhost=$DB_HOST \
+                     --allow-root \
+                     --path="/var/www/html"
+fi
 
 wp core install --url=$DOMAIN_NAME \
 				--title="my website" \
 				--admin_user="yassine" \
-				--admin_password="1234" \
-				--admin_email="yassineelhajjami610@gmail.com" \
+                --skip-email \
 				--allow-root \
-				--path="/var/www/html"
-
-# wp user create	$WP_USER $WP_EMAIL \
-# 				--user_pass=$WP_PASS \
-# 				--role=$WP_ROLE \
-# 				--allow-root
+				--admin_password="1234" \
+				--admin_email="yassineelhajjami610@gmail.com" --path="/var/www/html"
 
 /usr/sbin/php-fpm7.4 -F
